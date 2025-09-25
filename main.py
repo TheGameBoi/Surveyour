f88516bc81a684a62f7f120e90839692a9631768
GameBoi <thegameboi@proton.me>
2025-09-24 20:16:59 -0500

    Added a New Survey button and the Survey Class.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import (QComboBox, QMessageBox, QPushButton, QLabel, QLineEdit, QGridLayout, QApplication,
-    QMainWindow, QGridLayout, QWidget, QTableWidget, QToolBar, QTextEdit, QStatusBar,
-    QTableWidgetItem)
+        QMainWindow, QGridLayout, QWidget, QTableWidget, QToolBar, QTextEdit, QStatusBar,
+        QTableWidgetItem, QDialog)
 from PyQt6.QtGui import QIcon, QAction
 from PyQt6.QtCore import Qt
 import mysql.connector
@@ -23,6 +23,52 @@ class MainWindow(QMainWindow):
         self.table.setHorizontalHeaderLabels(("Name", "Age", "Gender", "City"))
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
+
+
+        new_survey = QAction("New Survey", self)
+
+
+
+        # Toolbar
+        toolbar = QToolBar(self)
+        toolbar.setMovable(True)
+        self.addToolBar(toolbar)
+        toolbar.addAction(new_survey)
+
+
+
+
+class SurveyDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Add Data")
+        self.setFixedSize(600, 400)
+
+        # Grid layout
+        layout = QGridLayout()
+
+        # Name
+        self.name = QLineEdit()
+        self.name.setPlaceholderText("Name")
+
+        # Age
+        self.age = QLineEdit()
+        self.age.setPlaceholderText("Age")
+
+        # Gender
+        self.gender = QLineEdit()
+        self.gender.setPlaceholderText("Gender")
+
+        # City
+        self.city = QLineEdit()
+        self.city.setPlaceholderText("City")
+
+        # Submit Button
+        add = QPushButton("Submit")
+
+
+        # Setting Layout
+        self.setLayout(layout)
 
 
 app = QApplication(sys.argv)
